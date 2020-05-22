762ccdac3a8de0bfcd0786d50c3c5ff5c0bfe839
Keith Hughitt <keith.hughitt@gmail.com>
2020-05-21 20:10:49 -0400

    Added PCA/UMAP plots of covariates; switched from feather -> arrow library for feather file i/o

---
+++ Snakefile
@@ -0,0 +1,64 @@
+"""
+MM25 Score generation pipeline
+
+KH 2020.02.02
+
+Combines output from fassoc feature-phenotype association pipeline to generate final
+MM25 gene and pathway weights.
+"""
+import os
+import pandas as pd
+
+# base input and output data dirs;
+# output comes from the separate feature association ("fassoc") pipeline
+input_dir = os.path.join(config["fassoc_dir"], "mm25", config["version"])
+out_dir = os.path.join(config["out_dir"], config["version"])
+
+# load phenotype metadata
+phenotypes_infile = os.path.join(input_dir, "metadata", "association_metadata.feather")
+phenotypes = pd.read_feather(phenotypes_infile)
+
+# phenotype categories
+categories = phenotypes.category.unique()
+
+# filepath for one of the custom gmt outputs
+custom_gmt = os.path.join(out_dir, "gmt", "mm25_top_{}_pathways.gmt".format(config["gene_sets"]["sizes"][0]))
+
+wildcard_constraints:
+    category="|".join(categories)
+
+rule all:
+    input:
+        expand(os.path.join(out_dir, "all", "mm25_{feat_level}_scores.feather"), 
+               feat_level=["gene", "pathway"]),
+        expand(os.path.join(out_dir, "targeted", "mm25_{feat_level}_{category}_scores.feather"), 
+               feat_level=["gene", "pathway"], category=categories),
+        custom_gmt
+
+rule create_custom_gene_sets:
+    input:
+        os.path.join(out_dir, "all", "mm25_pathway_scores.feather") 
+    output:
+        custom_gmt
+    script:
+        "src/create_custom_gmt.R"
+
+rule mm25_targeted:
+    input: 
+        pvals=os.path.join(input_dir, "merged", "mm25_{feat_level}_association_pvals.feather"),
+        stats=os.path.join(input_dir, "merged", "mm25_{feat_level}_association_stats.feather")
+    output:
+        os.path.join(out_dir, "targeted", "mm25_{feat_level}_{category}_scores.feather")
+    params:
+        category="{category}"
+    script:
+        "src/build_scores.R"
+
+rule mm25_all:
+    input: 
+        pvals=os.path.join(input_dir, "merged", "mm25_{feat_level}_association_pvals.feather"),
+        stats=os.path.join(input_dir, "merged", "mm25_{feat_level}_association_stats.feather")
+    output:
+        os.path.join(out_dir, "all", "mm25_{feat_level}_scores.feather")
+    script:
+        "src/build_scores.R"
---
+++ src/create_custom_gmt.R
@@ -0,0 +1,111 @@
+#!/bin/env Rscript
+#
+# Creates a custom filtered gmt file containing the top N highest ranking MM25
+# pathways/gene sets.
+#
+suppressMessages(library(GSEABase))
+suppressMessages(library(arrow))
+suppressMessages(library(tidyverse))
+
+options(stringsAsFactors = FALSE)
+
+# largest custom gmt file to generate (# gene sets)
+max_gmt_size <- max(snakemake@config$gene_sets$sizes)
+
+# load MM25 pathway rankings
+gene_set_ranks <- read_feather(snakemake@input[[1]])
+gene_set_ranks$gene_set <- as.character(gene_set_ranks$gene_set)
+
+# grab top N gene sets (already in sorted order)
+top_gene_sets <- gene_set_ranks %>%
+  head(max_gmt_size) %>%
+  pull(gene_set)
+
+# load input gmts
+gmt_paths <- Sys.glob(snakemake@config$gene_sets$gmts)
+
+gene_sets <- lapply(gmt_paths, function(x) {
+  res <- geneIds(getGmt(x))
+  lapply(res, function(gset) { gset[gset != ''] })
+})
+names(gene_sets) <- tools::file_path_sans_ext(basename(gmt_paths))
+
+# remove gene set :length suffixes, if present
+names(gene_sets) <- sub(':\\d+$', '', names(gene_sets))
+
+# remove any leading or trailing whitespace in gene set names;
+# encountered at least one instance of this ("HEK 293 T-rex" in NCI-60 gene set
+# collection)
+for (gset in names(gene_sets)) {
+  names(gene_sets[[gset]]) <- trimws(names(gene_sets[[gset]]))
+}
+
+# remove gene set weights, if present
+# e.g. "ANXA1,1.0" -> "ANXA1"
+for (gset in names(gene_sets)) {
+  gene_sets[[gset]] <- lapply(gene_sets[[gset]], function(x) { sub(',\\d+\\.\\d+$', '', x) })
+}
+
+# create a mapping from <collection, gene_set> pairs to the merged "<collection>_<gene_set>"
+# identifiers used in MM25
+mapping <- NULL
+
+for (collection in names(gene_sets)) {
+  mapping <- rbind(mapping, cbind(collection, gene_set = names(gene_sets[[collection]])))
+}
+mapping <- as.data.frame(mapping, stringsAsFactors = FALSE)
+
+mapping$combined_id <- paste(mapping$collection, mapping$gene_set, sep = '_')
+
+# collapse nested collection/gene set list into a single list indexed
+# by combined <collection>_<gene_set> identifiers to make it easier to find
+# entries using the MM25 combined identifiers
+combined_gene_sets <- list()
+
+for (collection in names(gene_sets)) {
+  collection_list <- gene_sets[[collection]]
+  names(collection_list) <- paste(collection, names(collection_list), sep = '_')
+
+  combined_gene_sets <- c(combined_gene_sets, collection_list)
+}
+
+# order by MM25 gene set rank and limit to size of largest custom gmt to be created
+combined_gene_sets <- combined_gene_sets[top_gene_sets]
+
+max_genes <- max(unlist(lapply(combined_gene_sets, length)))
+
+# dataframe to store gmt output; 2 added to account for collection / gene set
+# id columns
+res <- matrix("", nrow = length(combined_gene_sets), ncol = max_genes + 2)
+
+# iterate over top gene sets and add to custom GMT file
+for (i in 1:length(combined_gene_sets)) {
+    combined_id <- names(combined_gene_sets)[i]
+
+    # split combined collection / gene set identifier back into individual components
+    ind <- match(combined_id, mapping$combined_id)
+
+    collection <- mapping$collection[ind]
+    gene_set <- mapping$gene_set[ind]
+
+    # get genes in gene set
+    genes <- combined_gene_sets[[combined_id]]
+
+    res[i, 1:(2 + length(genes))] <- c(gene_set, collection, genes)
+}
+
+# iterate over entries and write to file
+out_dir <- dirname(snakemake@output[[1]])
+
+for (num_gene_sets in snakemake@config$gene_sets$sizes) {
+  outfile <- file.path(out_dir, sprintf('mm25_top_%d_pathways.gmt', num_gene_sets))
+
+  # get the first N entries 
+  output <- apply(res[1:num_gene_sets, ], 1, function(x) { 
+    x <- x[x != '']
+    paste(x, collapse = '\t')
+  })
+
+  # save custom gmt
+  write(output, file = outfile)
+}
