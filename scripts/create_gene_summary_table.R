123556cd57085f2c9d42c6c8c61472b888ee4d4e
Keith Hughitt <keith.hughitt@gmail.com>
2024-12-05 07:26:14 -0500

    add metafor results to gene-level summary table

--- scripts/create_gene_summary_table.R
+++ scripts/create_gene_summary_table.R
@@ -13,27 +13,37 @@ ratio_nonzeros <- read_feather(snakemake@input[[5]])
 scores_all <- read_feather(snakemake@input[[6]]) %>%
   mutate(rank_all_sumz_wt = dense_rank(sumz_wt_pval),
          rank_all_sumz = dense_rank(sumz_pval)) %>%
-  select(symbol, rank_all_sumz_wt, rank_all_sumz)
+  select(symbol, rank_all_sumz_wt, rank_all_sumz, 
+         num_present_total = num_present, 
+         num_missing_total = num_missing)
 
 scores_stage <- read_feather(snakemake@input[[7]]) %>%
   mutate(rank_stage_sumz_wt = dense_rank(sumz_wt_pval),
-         rank_stage_sumz = dense_rank(sumz_pval)) %>%
-  select(symbol, rank_stage_sumz_wt, rank_stage_sumz)
+         rank_stage_sumz = dense_rank(sumz_pval),
+         rank_stage_metafor = dense_rank(metafor_pval)) %>%
+  select(symbol, rank_stage_sumz_wt, rank_stage_sumz, rank_stage_metafor,
+         num_stage = num_present)
 
 scores_surv_os <- read_feather(snakemake@input[[8]]) %>%
   mutate(rank_surv_os_sumz_wt = dense_rank(sumz_wt_pval),
-         rank_surv_os_sumz = dense_rank(sumz_pval)) %>%
-  select(symbol, rank_surv_os_sumz_wt, rank_surv_os_sumz)
+         rank_surv_os_sumz = dense_rank(sumz_pval),
+         rank_surv_os_metafor = dense_rank(metafor_pval)) %>%
+  select(symbol, rank_surv_os_sumz_wt, rank_surv_os_sumz, rank_surv_os_metafor,
+         num_surv_os = num_present)
 
 scores_surv_pfs <- read_feather(snakemake@input[[9]]) %>%
   mutate(rank_surv_pfs_sumz_wt = dense_rank(sumz_wt_pval),
-         rank_surv_pfs_sumz = dense_rank(sumz_pval)) %>%
-  select(symbol, rank_surv_pfs_sumz_wt, rank_surv_pfs_sumz)
+         rank_surv_pfs_sumz = dense_rank(sumz_pval),
+         rank_surv_pfs_metafor = dense_rank(metafor_pval)) %>%
+  select(symbol, rank_surv_pfs_sumz_wt, rank_surv_pfs_sumz, rank_surv_pfs_metafor,
+         num_surv_pfs = num_present)
 
 scores_trmt <- read_feather(snakemake@input[[10]]) %>%
   mutate(rank_trmt_sumz_wt = dense_rank(sumz_wt_pval),
-         rank_trmt_sumz = dense_rank(sumz_pval)) %>%
-  select(symbol, rank_trmt_sumz_wt, rank_trmt_sumz)
+         rank_trmt_sumz = dense_rank(sumz_pval),
+         rank_trmt_metafor = dense_rank(metafor_pval)) %>%
+  select(symbol, rank_trmt_sumz_wt, rank_trmt_sumz, rank_trmt_metafor, 
+         num_trmt = num_present)
 
 ratio_na <- apply(means[, -1], 1, function(x) {
   sum(is.na(x)) / length(x)
